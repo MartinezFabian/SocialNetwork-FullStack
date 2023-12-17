72402e1e1ba129b5a4b838dd60587a8cab8a805c
MartinezFabian <FabianMartinezDev@gmail.com>
2023-12-16 21:37:06 -0300

    Feat: Implement 'deletePost' controller to remove a post from 'posts' table

--- api/src/routes/posts.js
+++ api/src/routes/posts.js
@@ -1,9 +1,10 @@
 import Express from 'express';
-import { addPost, getPost } from '../controllers/postController.js';
+import { addPost, getPost, deletePost } from '../controllers/postController.js';
 
 const router = Express.Router();
 
 router.post('/', addPost);
 router.get('/', getPost);
+router.delete('/:id', deletePost);
 
 export default router;
