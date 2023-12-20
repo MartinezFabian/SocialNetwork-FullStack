c99c5d3e976cb2e2c09c1da8d834a003bb3934e6
MartinezFabian <FabianMartinezDev@gmail.com>
2023-12-20 20:38:27 -0300

    Feat: Add LoginPage.module.css for styling Login component

--- client/src/auth/pages/LoginPage.jsx
+++ client/src/auth/pages/LoginPage.jsx
@@ -1,27 +1,45 @@
+import styles from './LoginPage.module.css';
+
 export const LoginPage = () => {
   return (
-    <main>
-      <div>
-        <h1>Sign in</h1>
+    <main className={styles.card}>
+      <h1 className={styles.title}>Sign in</h1>
 
-        <div>
-          <form>
-            <div>
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" name="username" placeholder="username" />
-            </div>
+      <div className={styles.content}>
+        <form className={styles.form}>
+          <div className={styles.field}>
+            <label htmlFor="username" className={styles.label}>
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="username"
+              className={styles.input}
+            />
+          </div>
 
-            <div>
-              <label htmlFor="password">Password</label>
-              <input type="text" id="password" name="password" placeholder="password" />
-            </div>
+          <div className={styles.field}>
+            <label htmlFor="password" className={styles.label}>
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password"
+              className={styles.input}
+            />
+          </div>
 
-            <div>
-              <button type="submit">Sign in</button>
-              <button>Register</button>
-            </div>
-          </form>
-        </div>
+          <div className={styles.buttons}>
+            <button type="submit" className={styles.button}>
+              Sign in
+            </button>
+            <button className={styles.button}>Register</button>
+          </div>
+        </form>
       </div>
     </main>
   );
