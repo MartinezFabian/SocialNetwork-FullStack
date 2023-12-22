57cfc10928eaf2b5e81ab38e8b63777ad8bcdb72
MartinezFabian <FabianMartinezDev@gmail.com>
2023-12-21 21:14:19 -0300

    Feat: Add basic structure for LeftBar.jsx and LeftBar.module.css

--- client/src/ui/LeftBar.jsx
+++ client/src/ui/LeftBar.jsx
@@ -1,7 +1,32 @@
+import { NavLink } from 'react-router-dom';
+import styles from './LeftBar.module.css';
+
 export const LeftBar = () => {
   return (
-    <aside>
-      <h1>LeftBar</h1>
+    <aside className={styles.aside}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <NavLink
+            exact
+            to="/"
+            className={({ isActive }) =>
+              isActive ? `${styles.link} ${styles.active} ` : styles.link
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className={styles.item}>
+          <NavLink
+            to="/profile/1"
+            className={({ isActive }) =>
+              isActive ? `${styles.link} ${styles.active} ` : styles.link
+            }
+          >
+            Profile
+          </NavLink>
+        </li>
+      </ul>
     </aside>
   );
 };
