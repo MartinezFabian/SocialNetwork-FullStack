9e5bf80a154c39466df6efb92d008fb4c4cb656f
MartinezFabian <FabianMartinezDev@gmail.com>
2023-12-23 20:56:35 -0300

    Feat: Add changes in CSS styles for responsive design

--- client/src/ui/Layout.jsx
+++ client/src/ui/Layout.jsx
@@ -2,17 +2,21 @@ import { Outlet } from 'react-router-dom';
 import { LeftBar } from './LeftBar';
 import { Navbar } from './Navbar';
 import { RightBar } from './RightBar';
+import styles from './Layout.module.css';
 
 export const Layout = () => {
   return (
     <>
       <Navbar></Navbar>
-      <div style={{ display: 'flex' }}>
+      <div className={styles.container}>
         <LeftBar />
-        <main style={{ flex: 6 }}>
-          <Outlet />
-        </main>
-        <RightBar />
+
+        <div className={styles.mainContainer}>
+          <main className={styles.main}>
+            <Outlet />
+          </main>
+          <RightBar />
+        </div>
       </div>
     </>
   );
