b02047732ee5551f27963d10bc1801441383e3f8
MartinezFabian <FabianMartinezDev@gmail.com>
2023-12-26 21:06:50 -0300

    Feat: Add form validation in RegisterPage.jsx

--- client/src/auth/pages/RegisterPage.jsx
+++ client/src/auth/pages/RegisterPage.jsx
@@ -1,17 +1,44 @@
+import { useForm } from 'react-hook-form';
 import styles from './RegisterPage.module.css';
 
 export const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm();
+
+  const onFormSubmit = handleSubmit((data) => {
+    console.log(data);
+
+    reset();
+  });
+
   return (
     <main className={styles.card}>
       <h1 className={styles.title}>Register</h1>
 
       <div className={styles.content}>
-        <form className={styles.form}>
+        <form onSubmit={onFormSubmit} className={styles.form}>
           <div className={styles.field}>
             <label htmlFor="name" className={styles.label}>
               Full name
             </label>
-            <input type="name" id="name" name="name" placeholder="name" className={styles.input} />
+            <input
+              type="name"
+              id="name"
+              placeholder="name"
+              name="name"
+              {...register('name', {
+                required: { value: true, message: 'Name is required' },
+                maxLength: { value: 23, message: 'Name must not exceed 23 characters' },
+                minLength: { value: 3, message: 'Name must be longer than 3 characters' },
+              })}
+              className={styles.input}
+            />
+            {errors.name ? <span className={styles.error}>{errors.name.message}</span> : null}
           </div>
 
           <div className={styles.field}>
@@ -21,10 +48,18 @@ export const RegisterPage = () => {
             <input
               type="text"
               id="username"
-              name="username"
               placeholder="username"
+              name="username"
+              {...register('username', {
+                required: { value: true, message: 'Username is required' },
+                maxLength: { value: 16, message: 'Username must not exceed 16 characters' },
+                minLength: { value: 3, message: 'Username must be longer than 3 characters' },
+              })}
               className={styles.input}
             />
+            {errors.username ? (
+              <span className={styles.error}>{errors.username.message}</span>
+            ) : null}
           </div>
 
           <div className={styles.field}>
@@ -34,10 +69,18 @@ export const RegisterPage = () => {
             <input
               type="email"
               id="email"
-              name="email"
               placeholder="email"
+              name="email"
+              {...register('email', {
+                required: { value: true, message: 'Email is required' },
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: 'Email is not valid',
+                },
+              })}
               className={styles.input}
             />
+            {errors.email ? <span className={styles.error}>{errors.email.message}</span> : null}
           </div>
 
           <div className={styles.field}>
@@ -47,10 +90,21 @@ export const RegisterPage = () => {
             <input
               type="password"
               id="password"
-              name="password"
               placeholder="password"
+              name="password"
+              {...register('password', {
+                required: { value: true, message: 'Password is required' },
+                minLength: {
+                  value: 6,
+                  message: 'Password must be longer than 6 characters',
+                },
+              })}
               className={styles.input}
             />
+
+            {errors.password ? (
+              <span className={styles.error}>{errors.password.message}</span>
+            ) : null}
           </div>
 
           <div className={styles.field}>
@@ -60,10 +114,19 @@ export const RegisterPage = () => {
             <input
               type="password"
               id="confirmPassword"
-              name="confirmPassword"
               placeholder="confirm password"
+              name="confirmPassword"
+              {...register('confirmPassword', {
+                required: { value: true, message: 'Confirm Password is required' },
+                validate: (value) =>
+                  value === watch('password') ? true : 'Passwords do not match ',
+              })}
               className={styles.input}
             />
+
+            {errors.confirmPassword ? (
+              <span className={styles.error}>{errors.confirmPassword.message}</span>
+            ) : null}
           </div>
 
           <div className={styles.buttons}>
